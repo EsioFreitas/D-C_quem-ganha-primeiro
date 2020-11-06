c6942e87d81a583c2d142698750c6f4df84dd82e
phe0 <pheofo@gmail.com>
2020-11-06 11:40:29 -0300

    add swap and current trader

--- src/App.js
+++ src/App.js
@@ -2,33 +2,48 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import generateArray from './algorithm/generateArray';
 import mergeSort from './algorithm/mergeSort';
+import swap from './algorithm/swapArray';
 
 function App() {
 
   const [array, setArray] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(0)
+  const [selectedUser, setSelectedUser] = useState(null)
 
   useEffect(() => {
     reset()
   }, [])
 
   function solveProblem() {
-    const { arr: result , count, trades } = mergeSort({ arr: array, count: 0, trades: [] })
-    console.log(result)
-    console.log(count)
-    console.log(trades)
+    const { trades } = mergeSort({ arr: array, count: 0, trades: [] })
+    let count = 0;
+    let temp = [...array];
+    console.log(array)
+
+    const interval = setInterval(() => {
+      if (count >= trades.length) {
+        console.log('cabou')
+        clearInterval(interval)
+      } else {
+        setSelectedUser(count % 3);
+        temp = swap(temp, trades[count][0], trades[count][1])
+        console.log(temp)
+        setArray(temp)
+        count++;
+      }
+    }, 1000)
   }
 
   function reset() {
+    setSelectedUser(null);
     setArray(generateArray())
   }
 
   return (
     <div className="App">
       <ul>
-        <li>Andrea</li>
-        <li>Carlos</li>
-        <li>Marcelo</li>
+        <li style={ selectedUser === 0 ? { color: 'red', fontWeight: 'bold' } : null}>Andrea</li>
+        <li style={ selectedUser === 1 ? { color: 'red', fontWeight: 'bold' } : null}>Carlos</li>
+        <li style={ selectedUser === 2 ? { color: 'red', fontWeight: 'bold' } : null}>Marcelo</li>
       </ul>
       <ul>
         {
