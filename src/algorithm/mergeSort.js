85bb51368d03537415b82b32c23229dae0bf197b
phe0 <pheofo@gmail.com>
2020-11-06 10:29:00 -0300

    add mergesort function

---
+++ src/algorithm/mergeSort.js
@@ -0,0 +1,12 @@
+import merge from './merge';
+
+export default function mergeSort(obj) {
+    if(obj.arr.length === 1) {
+        return obj;
+    }
+    let middle = Math.floor(obj.arr.length / 2);
+    let left = { arr: obj.arr.slice(0, middle), count: obj.count, trades: obj.trades }
+    let right = { arr: obj.arr.slice(middle), count: obj.count, trades: obj.trades }
+    let result = merge(mergeSort(left), mergeSort(right))
+    return result;
+}
